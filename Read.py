cd7cfd845c681df007ec6f70b7351c84696342c1
Hau5ratz <16449790+Hau5ratz@users.noreply.github.com>
2024-08-14 08:43:22 +0100

    Create Read.py

---
+++ Read.py
@@ -0,0 +1,32 @@
+import openpyxl
+import pptx
+from pptx import Presentation
+from pptx.util import Pt
+import os
+from win32com.client import Dispatch
+import win32com
+from PIL import ImageGrab
+import copy
+import time
+import pyperclip
+import datetime
+import tkinter
+import tkinter.messagebox as mb
+from tkinter import filedialog as fd
+from openpyxl_image_loader import SheetImageLoader
+import sys
+import traceback
+
+# Open an excel file
+#workbook = Workbook("Book2.xlsx")
+
+__author__ = ""
+__copyright__ = None
+__credits__ = [""]
+__license__ = None
+__version__ = "2.0.0"
+__maintainer__ = ""
+__email__ = ""
+__status__ = "Development"
+
+
